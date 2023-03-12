e4265ccafd0c42c63259f8aac10dae6249a15f4b
cuneyt AYKAÇ <cuneyt.aykac@indbilisim.com.tr>
2023-03-12 18:05:43 +0300

    work completed.

--- UIViewController_life_cycle/ViewController.swift
+++ UIViewController_life_cycle/ViewController.swift
@@ -5,15 +5,53 @@
 //  Created by Cuneyt on 12.03.2023.
 //
 
+/**
+ 
+ UIViewController Life Cycle
+ 
+ 
+    1- init
+    2-loadView
+    3-viewDidLoad
+    4-viewWillAppear
+    5-viewDidAppear
+    6-viewWillDisAppear
+    7-viewDidDisAppear
+
+ 
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
+   
 
     override func viewDidLoad() {
+        
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad worked")
     }
+    
+ 
 
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear worked")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear worked")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear worked")
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear worked")
+    }
 
 }
 
---
+++ UIViewController_life_cycle/ViewControllerTwo.swift
@@ -0,0 +1,24 @@
+//
+//  ViewControllerTwo.swift
+//  UIViewController_life_cycle
+//
+//  Created by Cuneyt on 12.03.2023.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+    @IBAction func back(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+
+}
