e12208e3210da7f79ae7b6c611e5e4f6dcbba749
cuneyt AYKAÇ <cuneyt.aykac@indbilisim.com.tr>
2023-03-12 16:41:28 +0300

    Initial Commit

---
+++ UIViewController_life_cycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIViewController_life_cycle
+//
+//  Created by Cuneyt on 12.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
